3fa4f747984a04da681fe23de74b0ee72b964a11
Jiseung <roadrunner23@yonsei.ac.kr>
2020-12-31 20:01:16 +0900

    Feat: Header, Main, Footer Seperation
    - Add logo and shopping bag icon to header
    - Add icon attribution line to footer

--- app/settings.py
+++ app/settings.py
@@ -137,6 +137,6 @@ AWS_STORAGE_BUCKET_NAME = "jiseung-bucket"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_S3_REGION_NAME = "ap-northeast-2"
