b35501538b80d5cadd48759f60f0a546179fb12e
Jiseung <roadrunner23@yonsei.ac.kr>
2021-02-05 20:30:12 +0900

    Fix: Create 12 slots, Change font and button style
    TODO: Redundant pokeball svg elements. Write script to clone elements.

--- static/js/pokeballAnimation.js
+++ static/js/pokeballAnimation.js
@@ -1,4 +1,5 @@
 const pokeballs = Array.from(document.querySelectorAll('.pokeball'));
+const pokeballOpen = Array.from(document.querySelector('.pokeball-open').children)
 const pokeballClosed = Array.from(document.querySelector('.pokeball-closed').children);
 const speechMessages = Array.from(document.querySelectorAll('.speech-message'))
 const speechBubbles = Array.from(document.querySelectorAll('.speech-bubble'))
@@ -6,27 +7,34 @@ const speechBubbles = Array.from(document.querySelectorAll('.speech-bubble'))
 gsap.registerPlugin(MorphSVGPlugin);
 
 for (const pokeball of pokeballs) {
-    pokeball.open = true;
+    pokeball.open = true
     pokeball.addEventListener('click', (e) => {
         const slot = e.target.closest('.break-slot')
-        const paths = Array.from(slot.querySelectorAll('.pokeball path'))
+        const ball = slot.querySelector('.pokeball');
+        const paths = Array.from(ball.querySelectorAll('path'))
+        
+        // Pokeball Animation
         paths.map((item, idx) => {
             if (idx == 5)
                 shapeIndex = 0; 
-            else if (idx == 6) 
+            else if (idx == 6 && pokeball.open) 
                 shapeIndex = 1;
             else
                 shapeIndex = "auto";
             gsap.to(item, {
                 morphSVG: {
-                    shape: pokeballClosed[idx],
+                    shape: ball.open ? pokeballClosed[idx] : pokeballOpen[idx],
                     shapeIndex: shapeIndex
                 }
             });
         })
         
-        // gsap.to(slot.querySelector('.speech-message'), {transform: 'translate(-2.5rem, -1.55rem) scale(0)'})
-        gsap.to(slot.querySelector('.speech-bubble'), {transform: 'translate(-2.5rem, -1.55rem) scale(0)'})
+        // Speech Bubble Animation
+        gsap.to(slot.querySelector('.speech-bubble'), {
+            transform: ball.open ? 'translate(-2.5rem, -1.55rem) scale(0)' : 'translate(-2.5rem, -2rem) scale(1)'
+        })
+        
+        ball.open = !ball.open;
     })
 }
 
