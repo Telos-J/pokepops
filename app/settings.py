e09732f701760f5d84417451294eb3ad8c4971f3
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-04-26 13:22:01 +0900

    Fix: database settings

--- app/settings.py
+++ app/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
+import dj_database_url
 
-# DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Authentication
 
